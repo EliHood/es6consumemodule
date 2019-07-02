5e4696e67eca09f4b956cbec8c8aa5163d2c612e
Eli <elihood@Elis-MacBook-Pro.local>
2019-07-02 11:43:48 -0500

    update

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 import * as myModule from './src/'
 
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
+const url = 'https://jsonplaceholder.typicode.com/posts';
 const value = {name:"Bob", job:'Software Dev'};
 const config = {
-    method: 'DELETE',
+    method: 'GET',
     url:url,
     // data: {...value},
+}
+
+const config2 = {
+    method: 'POST',
+    url:url,
+    data: {...value},
 
 }
 
-console.log(test);
+
+const test = myModule.fetch(config).then( res => {
+    console.log(res)
+}).catch(err => {
+    console.log(err);
+})
+
+const test2 = myModule.fetch(config2).then( res => {
+    console.log(res)
+}).catch(err => {
+    console.log(err);
+})
+
+
+
+console.log(test + test2);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 'use strict';
 import axios from 'axios';
 import log from 'log-to-file';
+
 /**
  * Makes axios call
  * @param {params} object
  *
 */
+
+let logger = []
+
+
 axios.interceptors.request.use( (config) =>{
     config.metadata = {
         startTime: Date.now()
@@ -18,11 +23,35 @@ axios.interceptors.request.use( (config) =>{
 axios.interceptors.response.use( (response) => {
     response.config.metadata.endTime = Date.now()
     response.duration = response.config.metadata.endTime - response.config.metadata.startTime
+    
+    
+    const time = {
+        startTime: response.config.metadata.startTime,
+        endTime: response.config.metadata.endTime,
+        duration: response.duration,
+        url: response.config.url,
+        method: response.config.method
+    }
+    if(logger.length > 80){
+        logger = [];
+    }
+    else {
+        logger.push(time);
+    }
+    
+    console.log(logger);
     return response;
 }, (error) => {
     return Promise.reject(error);
 })
 export async function fetch(params){
     const data = await axios(params);
-    return data;
+    return data.data;
+}
+
+
+export function  millSecondMinutes(time){
+    var minutes = Math.floor(time / 60000);
+    var seconds = ((time % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
