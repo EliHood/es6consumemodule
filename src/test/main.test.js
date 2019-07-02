90490b3affdd96a8bc595dd02372c1cf73b88205
Eli <elihood@Elis-MacBook-Pro.local>
2019-07-01 23:56:18 -0500

    update

---
+++ src/test/main.test.js
@@ -0,0 +1,25 @@
+import * as consume from '../index';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import chaiAsPromised from "chai-as-promised";
+// import request from 'request';
+const expect = chai.expect;
+
+
+chai.use(chaiHttp);
+chai.use(chaiAsPromised);
+chai.should();
+
+const assert = chai.assert;
+
+describe('should test GET', () => {
+    it('should test GET method', async (done) => {
+        const object = {
+            method: 'GET',
+            url:'https://jsonplaceholder.typicode.com/posts/1',
+        }
+        // expect(consume.fetch(object)).to.be.a('object');
+       const ourData = consume.fetch(object)
+       return assert.isFulfilled(ourData, "optional message");
+    })
+} )
