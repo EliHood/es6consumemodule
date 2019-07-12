3c14e0e915af175fc0d23d7e0744036828fa54ae
Eli Hood <eli.hood@aol.com>
2019-07-12 11:53:48 -0500

    unit-test

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -1,13 +1,10 @@
 import * as consume from '../index';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import sinon from 'sinon';
-import chaiAsPromised from "chai-as-promised";
 import { equal } from 'assert';
 // import request from 'request';
 const expect = chai.expect;
 chai.use(chaiHttp);
-chai.use(chaiAsPromised);
 chai.should();
 const assert = chai.assert;
 describe('should test GET', () => {
@@ -34,4 +31,28 @@ describe('should test GET', () => {
             expect(res.logger[1].status).to.equal(201);
         })
     })
+    it('should test PUT method', (done) => {
+        const objectPut = {
+            method:'put',
+            url:'https://jsonplaceholder.typicode.com/posts/1',
+            data: {name:"Bob"}
+        }
+        consume.fetch(objectPut).then( (res) => {
+            done();
+            expect(res.logger[2].method).to.equal('put');
+            expect(res.logger[2].status).to.equal(200);
+        })
+    })
+    it('should test DELETE method', (done) => {
+        const objectDelete = {
+            method:'delete',
+            url:'https://jsonplaceholder.typicode.com/posts/1',
+            data: {name:"Bob"}
+        }
+        consume.fetch(objectDelete).then( (res) => {
+            done();
+            expect(res.logger[3].method).to.equal('delete');
+            expect(res.logger[3].status).to.equal(200);
+        })
+    })    
 })
