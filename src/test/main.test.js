202d30e94b7d2939b76fd76915464d0618545f0e
Eli Hood <eli.hood@aol.com>
2019-07-12 11:41:52 -0500

    unit test

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -1,28 +1,37 @@
 import * as consume from '../index';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import sinon from 'sinon';
 import chaiAsPromised from "chai-as-promised";
+import { equal } from 'assert';
 // import request from 'request';
 const expect = chai.expect;
-
-
 chai.use(chaiHttp);
 chai.use(chaiAsPromised);
 chai.should();
-
 const assert = chai.assert;
-
 describe('should test GET', () => {
-    it('should test GET method', async (done) => {
-        const object = {
+    it('should test GET method', (done) => {
+        const objectGet = {
             method: 'GET',
             url:'https://jsonplaceholder.typicode.com/posts/1',
         }
-        const data = consume.fetch(object).then( res => {
-            return res.data
-        });
-        expect(data).to.be.a('object');
-    //    const ourData = consume.fetch(object)
-    //    return assert.isFulfilled(ourData, "optional message");
+        consume.fetch(objectGet).then( (res) => { 
+            done()  
+            expect(res.logger[0].status).to.equal(200);
+            expect(res.logger[0].method).to.equal('get');
+        })
     })
-} )
+    it('should test POST method', (done) => {
+        const objectPOST = {
+            method:'POST',
+            url:'https://jsonplaceholder.typicode.com/posts',
+            data: {name:"Bob"}
+        }
+        consume.fetch(objectPOST).then( (res) => {
+            done();
+            expect(res.logger[1].method).to.equal('post');
+            expect(res.logger[1].status).to.equal(201);
+        })
+    })
+})
